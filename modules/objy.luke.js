bea481bc6a629099257acb1f4ff1e09e2f988ad6
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2020-07-14 13:10:55 +0200

    fixed modules

--- modules/objy.luke.js
+++ modules/objy.luke.js
@@ -1,52 +1,55 @@
-module.exports = {
-    delimeter: ";",
-    assignmentOperator: "=",
-    context: {},
-    $: {
-        objy: {
-            define: {
-                follow: ["$objectFamily"],
-                method: function() {
-                    console.log('define');
-                }
-            },
-            add: {
-                follow: ["{name}", "$width"],
-                method: function(p) {
-                    console.log('add(' + p + ')');
-                }
-            },
-            obj: {
-                follow: ["$set"],
-                method: function() {
+var dsl = {
+    lang: {
+        delimeter: ";",
+        assignmentOperator: "=",
+        context: {},
+        $: {
+            objy: {
+                define: {
+                    follow: ["$objectFamily"],
+                    method: function() {
+                        console.log('define');
+                    }
+                },
+                add: {
+                    follow: ["{name}", "$width"],
+                    method: function(p) {
+                        console.log('add(' + p + ')');
+                    }
+                },
+                obj: {
+                    follow: ["$set"],
+                    method: function() {
 
-                }
-            },
-            objectFamily: {
-                follow: ["$set", "$width", "${ofName}", "{name}"],
-                method: function(p) {
-                    console.log('objectFamily(' + p + ')');
-                }
-            },
-            set: {
-                follow: ["{name}", "$set", "$and", "$with", "$exec"],
-                method: function(r) {
-                    //console.log(r);
-                    console.log('set(' + r + ')');
-                }
-            },
-            "width": {
-                follow: ["{name}", "$and"],
-                method: function(p) {
-                    console.log('width(' + p + ')');
-                }
-            },
-            and: {
-                follow: ["$set", "$width", "{sf}"],
-                method: function(p) {
-                    console.log('and', p);
+                    }
+                },
+                objectFamily: {
+                    follow: ["$set", "$width", "${ofName}", "{name}"],
+                    method: function(p) {
+                        console.log('objectFamily(' + p + ')');
+                    }
+                },
+                set: {
+                    follow: ["{name}", "$set", "$and", "$with", "$exec"],
+                    method: function(r) {
+                        //console.log(r);
+                        console.log('set(' + r + ')');
+                    }
+                },
+                "width": {
+                    follow: ["{name}", "$and"],
+                    method: function(p) {
+                        console.log('width(' + p + ')');
+                    }
+                },
+                and: {
+                    follow: ["$set", "$width", "{sf}"],
+                    method: function(p) {
+                        console.log('and', p);
+                    }
                 }
             }
         }
     }
 }
+module.exports = dsl;
