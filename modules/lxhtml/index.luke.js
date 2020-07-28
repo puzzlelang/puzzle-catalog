c5c534c54365102607eaa2ca139c5380a08f54e2
Marco Booelling <marco.boelling@web.de>
2020-07-28 20:47:32 +0200

    Update index.luke.js

--- modules/lxhtml/index.luke.js
+++ modules/lxhtml/index.luke.js
@@ -12,11 +12,12 @@ var syntax = {
             console.log(document, document.getElementById('html-canvas'));
             
             console.log(lxhtmlBus);
-            
+
             if(lxhtmlBus){
 
                 var data = {};
                 
+                console.log(syntax.context.html, syntax.context.html.substring(0,1))
                 if(syntax.context.html) data.html = syntax.context.html.substring(1, syntax.context.html.length-1);
                 if(syntax.context.style) data.style = syntax.context.style.substring(1, syntax.context.style.length-1);
                 if(syntax.context.js) data.js = syntax.context.js.substring(1, syntax.context.js.length-1);
