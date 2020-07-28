aff55fa482dbb502a0b5e1960258692d22269055
Marco Booelling <marco.boelling@web.de>
2020-07-28 19:51:36 +0200

    Update index.luke.js

--- modules/lxhtml/index.luke.js
+++ modules/lxhtml/index.luke.js
@@ -2,11 +2,27 @@ var syntax = {
     delimeter: ";",
     assignmentOperator: "=",
     context: {
-        attrs: {}
+        html: "",
+        style: "",
+        js: ""
     },
     static: {
         execStatement: function() {
             console.log('ctx', this.context)
+
+            if(document){
+                if(document.getElementById('html-canvas'))
+                {
+                    document.getElementById('html-canvas').innerHtml = this.context.html;
+                    document.getElementById('html-canvas').style = this.context.style;
+
+                    var script = document.createElement('script');
+                    var inlineCode = document.createTextNode(this.context.js);
+                    script.appendChild(inlineCode); 
+
+                    document.getElementById('html-canvas').appendChild(script);
+                }
+            }
         }
     },
     $: {
@@ -14,7 +30,19 @@ var syntax = {
             render: {
                 follow: ["{html}"],
                 method: function(ctx, html) {
-                    console.log('html', html);
+                    syntax.context.html = html;
+                }
+            },
+            style: {
+                follow: ["{style}"],
+                method: function(ctx, style) {
+                    syntax.context.style = style;
+                }
+            },
+            js: {
+                follow: ["{style}"],
+                method: function(ctx, style) {
+                    syntax.context.style = style;
                 }
             }
         }
