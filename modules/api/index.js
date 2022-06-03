3b9e048cb16eee50a60bd9506a72c5507d8bef55
Marco Boelling <marco.boelling@web.de>
2022-06-03 23:39:59 +0200

    added api module

---
+++ modules/api/index.js
@@ -0,0 +1,57 @@
+const Koa = require('koa');
+const router = require('@koa/router')();
+const app = new Koa();
+
+var syntax = {
+    $: {
+        api: {
+            _static: {
+                 execStatement: (done, ctx) => {
+
+                    if(ctx.method){
+                        router[ctx.method](ctx.path, _ctx => {
+                            global.puzzle.parse(ctx.code)
+                            _ctx.body = ctx.return;
+                        })
+
+                        app.use(router.routes());
+                    }
+                    done();
+                 }
+            },
+            start: {
+                follow: ["{port}"],
+                method: function(ctx, data){
+                    app.listen(data);
+                    console.log('listening on...', data)
+                }
+            },
+            on: {
+                follow: ["{method,path}", "$run"],
+                method: function(ctx, data) {
+                   ctx.method = data.method;
+                   ctx.path = data.path;
+                }
+            },
+            run: {
+                follow: ["{code}", "$and"],
+                method: function(ctx, data) {
+                   ctx.code = global.puzzle.getRawStatement(data);
+                }
+            },
+            return: {
+                follow: ["{code}", "%and"],
+                method: function(ctx, data) {
+                   ctx.return = global.puzzle.getRawStatement(data)
+                }
+            },
+            and: {
+                follow: ["$run", "$return"],
+                method: function(ctx, data) {
+                   
+                }
+            }
+        }
+    }
+}
+module.exports = syntax;
